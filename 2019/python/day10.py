52f955aa90275b89a82f27d5e8eb317a79c615cb
SeniorDerpyDev <SeniorDerpyDev@users.noreply.github.com>
2019-12-11 11:53:51 +0000

    Improvement to day 10 (python)

--- 2019/python/day10.py
+++ 2019/python/day10.py
@@ -1,25 +1,15 @@
-from math import (pi, atan2, gcd)
-from itertools import (repeat, product)
+from math import atan2, gcd
 
-def dir_sort_f(d):
+def key(d):
     x, y = d
-    r = atan2(y, x)
-    if (r < -pi/2):
-        r += 2*pi
-    return r
+    return -atan2(x, y)
 
 def scan(asteroid):
-    directions = set()
     x0, y0 = asteroid
-    for y,x in product(range(h), range(w)):
-        if (x, y) == (x0, y0):
-            continue
-        d = gcd(y - y0, x - x0)
-        dx, dy = (x - x0) // d, (y - y0) // d
-        directions.add((dx, dy))
+    directions = [(x - x0, y - y0) for y in range(h) for x in range(w) if gcd(x - x0, y - y0) == 1]
 
     visible = []
-    for (dx,dy) in sorted(directions, key=dir_sort_f):
+    for dx, dy in sorted(directions, key=key):
         xi, yi = x0 + dx, y0 + dy
         while xi >= 0 and xi < w and yi >= 0 and yi < h:
             if asteroid_map[yi][xi]:
@@ -31,22 +21,12 @@ def scan(asteroid):
 
 
 with open('../day_10.txt', 'r') as f:
-    m = [[c == '#' for c in line.strip()] for line in f.read().splitlines()]
+    asteroid_map = [[c == '#' for c in line.strip()] for line in f.read().splitlines()]
 
-asteroid_map, w, h = m, len(m[0]), len(m)
+w, h = len(asteroid_map[0]), len(asteroid_map)
 
-best_asteroid = None
-best_score = 0
-for y,x in product(range(h), range(w)):
-    if not asteroid_map[y][x]:
-        continue
-    score = len(scan((x,y)))
-    if score > best_score:
-        best_asteroid = (x,y)
-        best_score = score
+best = max((scan((x, y)) for y in range(h) for x in range(w) if asteroid_map[y][x]), key=len)
+print('part 1:', len(best))
 
-print('part 1:', best_score)
-
-targets = scan(best_asteroid)
-x200, y200 = targets[199]
-print('part 2:', x200*100 + y200)
+x200, y200 = best[199]
+print('part 2:', x200 * 100 + y200)
