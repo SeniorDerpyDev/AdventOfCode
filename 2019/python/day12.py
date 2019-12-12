2d97bb9b4da96a01a430e82435410381048634f0
SeniorDerpyDev <SeniorDerpyDev@users.noreply.github.com>
2019-12-12 15:10:26 +0000

    Day 12 (python)

---
+++ 2019/python/day12.py
@@ -0,0 +1,45 @@
+from itertools import combinations
+from math import gcd
+
+X_AXIS, Y_AXIS, Z_AXIS = 0, 1, 2
+
+# moon = [x, y, z, Vx, Vy, Vz]
+def get_initial_state():
+    return [ [10, 15, 7, 0, 0, 0], [15, 10, 0, 0, 0, 0], [20, 12, 3, 0, 0, 0], [0, -3, 13, 0, 0, 0] ]
+
+def simulate(moons, axes):
+    for (m0, m1) in combinations(moons, 2):
+        for axis in axes:
+            if m0[axis] < m1[axis]:
+                m0[axis+3] += 1
+                m1[axis+3] -= 1
+            elif m0[axis] > m1[axis]:
+                m0[axis+3] -= 1
+                m1[axis+3] += 1
+    for m in moons:
+        for axis in axes:
+            m[axis] += m[axis+3]
+
+moons = get_initial_state()
+for _ in range(1000):
+    simulate(moons, [X_AXIS, Y_AXIS, Z_AXIS])
+total_energy = sum( sum(abs(v) for v in m[0:3]) * sum(abs(v) for v in m[3:6]) for m in moons )
+print('part 1:', total_energy)
+
+def period(axis):
+    steps = 0
+    moons = get_initial_state()
+    state0 = [m[axis::3] for m in moons]
+    while True:
+        steps += 1
+        simulate(moons, [axis])
+        state = [m[axis::3] for m in moons]
+        if state == state0:
+            break
+    return steps
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+x, y, z = (period(axis) for axis in [X_AXIS, Y_AXIS, Z_AXIS])
+print('part 2:', lcm(lcm(x, y), z))
