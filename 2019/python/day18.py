0512e0196ef19924e5bd3b2e2bd82462fc70410e
SeniorDerpyDev <SeniorDerpyDev@users.noreply.github.com>
2020-01-13 11:10:20 +0000

    Improvements to day 18 (python)

--- 2019/python/day18.py
+++ 2019/python/day18.py
@@ -2,26 +2,19 @@ from heapq import heappop, heappush
 
 neighbours = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 
-def build_graph(maze):
+def build_graph(maze, starts):
     graph = dict()
-    starts = []
-    i = 0
-    w, h = len(maze[0]), len(maze)
-    for y in range(h):
-        for x in range(w):
-            c = maze[y][x]
-            if 'a' <= c <= 'z' or c == '@':
-                if c == '@':
-                    c += str(i)
-                    i += 1
-                    starts.append(c)
+    for y, row in enumerate(maze):
+        for x, c in enumerate(row):
+            if 'a' <= c <= 'z' or c in starts:
                 graph[c] = find_adjacent(maze, (x, y))
-    return graph, starts
+    return graph
 
 def find_adjacent(maze, start):
-    cells = [(*start, 0, set())]
-    visited = set()
+    visited = {start}
     adjacent = []
+    x, y = start
+    cells = [(x+dx, y+dy, 1, set()) for (dx, dy) in neighbours if maze[y+dy][x+dx] != '#']
 
     while cells:
         (x, y, steps, doors) = cells.pop(0)
@@ -30,46 +23,45 @@ def find_adjacent(maze, start):
         visited.add((x, y))
 
         c = maze[y][x]
-        if 'a' <= c <= 'z' and steps != 0:
+        if 'a' <= c <= 'z':
             adjacent.append((c, steps, doors))
             continue
         if 'A' <= c <= 'Z':
-            doors = doors | { c.lower() }
+            doors = doors | {c.lower()}
 
         cells.extend([(x+dx, y+dy, steps + 1, doors) for (dx, dy) in neighbours if maze[y+dy][x+dx] != '#'])
     return adjacent
 
 def get_all_keys(g, starts, keys_to_collect):
     q = []
-    heappush(q, (0, tuple(starts), frozenset()))
+    heappush(q, (0, starts, frozenset()))
     visited = set()
-    l = len(starts)
 
     while q:
         (steps, nodes, keys) = heappop(q)
-        if (nodes, keys) in visited:
-            continue
         if keys == keys_to_collect:
             return steps
-        for i in range(l):
-            lst = list(nodes)
-            for (next, dist, doors) in g[nodes[i]]:
+        if (nodes, keys) in visited:
+            continue
+        for n in nodes:
+            for next, dist, doors in g[n]:
                 if doors.issubset(keys):
-                    lst[i] = next
-                    heappush(q, (steps + dist, tuple(lst), keys | { next }))
+                    heappush(q, (steps + dist, nodes.replace(n, next), keys | {next}))
 
         visited.add((nodes, keys))
 
 with open('../day_18.txt', 'r') as f:
     maze = [list(line) for line in f.read().splitlines()]
 
-graph, starts = build_graph(maze)
+starts = '@'
+graph = build_graph(maze, starts)
 print('part 1:', get_all_keys(graph, starts, frozenset(graph.keys() - starts)))
 
 
-maze[39][39 : 42] = list('@#@')
+maze[39][39 : 42] = list('1#2')
 maze[40][39 : 42] = list('###')
-maze[41][39 : 42] = list('@#@')
+maze[41][39 : 42] = list('4#3')
 
-graph, starts = build_graph(maze)
+starts = '1234'
+graph = build_graph(maze, starts)
 print('part 2:', get_all_keys(graph, starts, frozenset(graph.keys() - starts)))
