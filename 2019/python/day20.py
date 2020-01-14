1024c2c3f744c4a3dae442e511a73bfbd69e1637
SeniorDerpyDev <SeniorDerpyDev@users.noreply.github.com>
2020-01-14 15:04:18 +0000

    Day 20 remake (python)

--- 2019/python/day20.py
+++ 2019/python/day20.py
@@ -1,103 +1,100 @@
-from collections import defaultdict
+import heapq
 
-adjacent = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+neighbours = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+OUTER, INNER = 0, 1
 
-def find_portals(m):
-    entries = defaultdict(list)
+def build_graph(m, start):
+    portals = dict()
     w, h = len(m[0]), len(m)
-
-    # horizontal portals
     for y in range(2, h - 2):
-        x = 0
-        while x < w - 1:
-            name = m[y][x] + m[y][x + 1]
-            if name.isalpha():
-                if m[y][x - 1] == '.':
-                    entries[name].append((x - 1, y))
-                    x += 1
-                else:
-                    entries[name].append((x + 2, y))
-                    x += 2
-            x += 1
+        for x in range(2, w - 2):
+            if m[y][x] != '.':
+                continue
 
-    # vertical portals
-    for x in range(2, w - 2):
-        y = 0
-        while y < h - 1:
-            name = m[y][x] + m[y + 1][x]
-            if name.isalpha():
-                if m[y - 1][x] == '.':
-                    entries[name].append((x, y - 1))
-                    y += 1
-                else:
-                    entries[name].append((x, y + 2))
-                    y += 2
-            y += 1
-
-    start = entries.pop('AA')[0]
-    end = entries.pop('ZZ')[0]
-    portals = {}
-    for name, entries in entries.items():
-        portals[entries[0]] = entries[1]
-        portals[entries[1]] = entries[0]
-
-    return portals, start, end
-
-def copy_maze(m):
-    return [list(l) for l in maze]
-
-def walk_maze(maze, portals, start, end):
-    maze = copy_maze(maze)
-    portals = dict(portals)
-    cells = [(*start, 0, False)]
-    while cells:
-        (x, y, s, warped) = cells.pop(0)
-
-        if (x, y) == end:
-            return s
-
-        if maze[y][x] != '.':
-            continue
-
-        if not warped and (x, y) in portals:
-            exit = portals[x, y]
-            cells.append((*exit, s + 1, True))
-        else:
-            cells.extend([(x + dx, y + dy, s + 1, False) for (dx, dy) in adjacent])
-
-        maze[y][x] = '+'
-
-def walk_maze_levels(maze, portals, start, end):
-    w, h = len(maze[0]), len(maze)
-    levels = {}
-    cells = [(*start, 0, False)]
-    while cells:
-        (x, y, l, s, warped) = cells.pop(0)
-
-        if (x, y, l) == end:
-            return s
-
-        m = levels.setdefault(l, copy_maze(maze))
-        if m[y][x] != '.':
-            continue
-
-        if not warped and (x, y) in portals:
-            exit = portals[x, y]
-            # outer portal
-            if x == 2 or y == 2 or x == w - 3 or y == h - 3:
-                if l > 0:
-                    cells.append((*exit, l - 1, s + 1, True))
+            if m[y][x-2:x].isalpha():
+                name = m[y][x-2:x]
+            elif m[y][x+1:x+3].isalpha():
+                name = m[y][x+1:x+3]
+            elif (m[y-2][x]+m[y-1][x]).isalpha():
+                name = m[y-2][x]+m[y-1][x]
+            elif (m[y+1][x]+m[y+2][x]).isalpha():
+                name = m[y+1][x]+m[y+2][x]
             else:
-                cells.append((*exit, l + 1, s + 1, True))
-        else:
-            cells.extend([(x + dx, y + dy, l, s + 1, False) for (dx, dy) in adjacent])
+                continue
 
-        m[y][x] = '+'
+            if name == start:
+                start_pos = (x, y)
+            elif x == 2 or y == 2 or x == w - 3 or y == h - 3:
+                portals[x,y] = (name, OUTER)
+            else:
+                portals[x,y] = (name, INNER)
+
+    g = {portal : find_adjacent(maze, portals, pos) for pos, portal in portals.items()}
+    g[start] = find_adjacent(maze, portals, start_pos)
+    return g
+
+def find_adjacent(maze, portals, pos):
+    visited = {pos}
+    adjacent = []
+    x, y = pos
+    cells = [(x+dx, y+dy, 1) for (dx, dy) in neighbours if maze[y+dy][x+dx] == '.']
+
+    while cells:
+        (x, y, steps) = cells.pop(0)
+        if (x, y) in visited:
+            continue
+        visited.add((x, y))
+
+        if (x, y) in portals:
+            adjacent.append((portals[x, y], steps))
+        else:
+            cells.extend([(x+dx, y+dy, steps + 1) for (dx, dy) in neighbours if maze[y+dy][x+dx] == '.'])
+    return adjacent
+
+def walk_maze(g, start, goal):
+    visited = {start}
+    q = [(dist, portal) for portal, dist in g[start]]
+    heapq.heapify(q)
+
+    while q:
+        (dist, portal) = heapq.heappop(q)
+        if portal[0] == goal:
+            return dist
+        if portal in visited:
+            continue
+
+        name, type = portal
+        for next, d in g[(name, 1-type)]:
+            heapq.heappush(q, (dist + d + 1, next))
+        visited.add(portal)
+
+def walk_maze_levels(g, start, goal):
+    visited = {(start, 0)}
+    q = [(dist, 0, portal) for portal, dist in g[start]]
+    heapq.heapify(q)
+
+    while q:
+        (dist, lvl, portal) = heapq.heappop(q)
+        if portal[0] == goal:
+            if lvl == 0:
+                return dist
+            else:
+                continue
+        if (portal, lvl) in visited:
+            continue
+
+        name, type = portal
+        for next, d in g[(name, 1-type)]:
+            if type == INNER:
+                heapq.heappush(q, (dist + d + 1, lvl + 1, next))
+            elif lvl > 0:
+                heapq.heappush(q, (dist + d + 1, lvl - 1, next))
+        visited.add((portal, lvl))
+
 
 with open('../day_20.txt', 'r') as f:
-    maze = [list(line) for line in f.read().splitlines()]
+    maze = f.read().splitlines()
 
-portals, start, end = find_portals(maze)
-
-print('part 1:', walk_maze(maze, portals, start, end))
-print('part 2:', walk_maze_levels(maze, portals, (*start, 0), (*end, 0)))
+g = build_graph(maze, 'AA')
+print('part 1:', walk_maze(g, 'AA', 'ZZ'))
+print('part 2:', walk_maze_levels(g, 'AA', 'ZZ'))
