90a73c99d11d9382fcddb788913fb75b5884ce3e
SeniorDerpyDev <SeniorDerpyDev@users.noreply.github.com>
2020-06-26 16:23:54 +0100

    Day 3 (rust)

---
+++ 2019/rust/aoc/src/day3.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+pub fn run(lines: &Vec<String>) {
+    let wire1 = parse(&lines[0]);
+    let wire2 = parse(&lines[1]);
+
+    let (r1, r2) = wire1
+        .keys()
+        .filter(|p| wire2.contains_key(p))
+        .map(|p| {
+            let (x, y) = p;
+            (x.abs() + y.abs(), wire1[p] + wire2[p])
+        })
+        .fold((i32::MAX, i32::MAX), |(a1, a2), (i1, i2)| {
+            (a1.min(i1), a2.min(i2))
+        });
+
+    println!("part 1: {}", r1);
+    println!("part 2: {}", r2);
+}
+
+fn parse(str: &String) -> HashMap<(i32, i32), i32> {
+    let mut points = HashMap::new();
+    let mut x = 0;
+    let mut y = 0;
+    let mut n = 0;
+
+    for s in str.split(',') {
+        let (l, r) = s.split_at(1);
+        let len = r.parse::<i32>().unwrap();
+        let (dx, dy) = match l {
+            "R" => (1, 0),
+            "L" => (-1, 0),
+            "U" => (0, 1),
+            "D" => (0, -1),
+            _ => panic!("invalid direction"),
+        };
+        for _ in 0..len {
+            x += dx;
+            y += dy;
+            n += 1;
+            points.entry((x, y)).or_insert(n);
+        }
+    }
+    points
+}
--- 2019/rust/aoc/src/main.rs
+++ 2019/rust/aoc/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
-use std::io::{BufReader,BufRead};
+use std::io::{BufRead, BufReader};
 
 mod day1;
+mod day3;
 
 fn read_input(path: String) -> std::io::Result<Vec<String>> {
     let file = File::open(path)?;
@@ -11,13 +12,15 @@ fn read_input(path: String) -> std::io::Result<Vec<String>> {
 
 fn main() {
     let arg = std::env::args().nth(1).expect("you must give me a day");
-    let path = std::env::args().nth(2).expect("you must give me a file path");
+    let path = std::env::args()
+        .nth(2)
+        .expect("you must give me a file path");
     let day = arg.parse::<i32>().expect("that's not a number");
     let lines = read_input(path).expect("there's something wrong with that path");
 
     match day {
         1 => day1::run(&lines),
-        _ => eprintln!("unknown day!")
+        3 => day3::run(&lines),
+        _ => eprintln!("unknown day!"),
     }
 }
-
