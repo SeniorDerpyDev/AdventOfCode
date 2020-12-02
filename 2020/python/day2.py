65218695c5f2207cb7e6faed8409c3621364e332
Luis Duarte <luis_duarte@outlook.com>
2020-12-02 10:26:49 +0000

    Day 2, 2020 (python)

---
+++ 2020/python/day2.py
@@ -0,0 +1,13 @@
+def parse(s):
+    [r, c, pwd] = s.split()
+    low, high = map(int, r.split('-'))
+    return (low, high, c[0], pwd)
+
+with open('../day_2.txt', 'r') as f:
+    passwords = list(parse(line.rstrip()) for line in f)
+
+p1 = sum(1 for low, high, c, pwd in passwords if low <= pwd.count(c) <= high)
+p2 = sum(1 for low, high, c, pwd in passwords if (pwd[low-1] == c) != (pwd[high-1] == c))
+
+print('part 1:', p1)
+print('part 1:', p2)
