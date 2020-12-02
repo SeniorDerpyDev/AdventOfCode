e8949fbfd94bc4c0e2d8d4b064824e8ba5d4f3ef
Luis Duarte <luis_duarte@outlook.com>
2020-12-02 18:19:19 +0000

    Day 1, 2020 improvement (python)

--- 2020/python/day1.py
+++ 2020/python/day1.py
@@ -1,12 +1,10 @@
-from itertools import combinations
-
 with open('../day_1.txt', 'r') as f:
     expenses = list(int(s.rstrip()) for s in f)
 
+target = 2020
 lookup = set(expenses)
-
-p1 = next(n*(2020-n) for n in expenses if 2020-n in lookup)
-p2 = next(l[0]*l[1]*l[2] for l in combinations(expenses, 3) if sum(l) == 2020) 
+p1 = next(n*(target-n) for n in expenses if target-n in lookup)
+p2 = next(n*m*(target-n-m) for i,n in enumerate(expenses) for m in expenses[i+1:] if target-n-m in lookup)
 
 print('part 1:', p1)
 print('part 1:', p2)
