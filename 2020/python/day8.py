fa7c85627cb20ae47366d81f07c556d763b36407
Luis Duarte <luis_duarte@outlook.com>
2020-12-08 15:16:09 +0000

    Day 8, 2020 (python)

---
+++ 2020/python/day8.py
@@ -0,0 +1,35 @@
+with open('../day_8.txt', 'r') as f:
+    pgm = [[op, int(n)] for op, n in (line.split() for line in f)]
+
+def run(pgm):
+    ip, acc = 0, 0
+    run = set()
+    while ip not in run and ip < len(pgm):
+        run.add(ip)
+        op, arg = pgm[ip]
+        if op == 'acc':
+            acc += arg
+            ip += 1
+        elif op == 'jmp':
+            ip += arg
+        else:
+            ip += 1
+    return acc, ip == len(pgm)
+
+
+def run_all(pgm):
+    for i, (op, arg) in enumerate(pgm):
+        if op == 'acc':
+            continue
+
+        fixed = list(pgm)
+        fixed[i] = ['nop', arg] if op == 'jmp' else ['jmp', arg]
+        acc, finished = run(fixed)
+        if finished:
+            return acc
+
+p1 = run(pgm)[0]
+p2 = run_all(pgm)
+
+print('part 1:', p1)
+print('part 2:', p2)
