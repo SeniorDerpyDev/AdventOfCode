4cc2895a0f35514b7d84d2f20eb752eb92ddf644
Luis Duarte <luis_duarte@outlook.com>
2020-12-08 15:32:14 +0000

    Day 7, 2020 improvement (python)

--- 2020/python/day7.py
+++ 2020/python/day7.py
@@ -1,7 +1,8 @@
+import re
+
 with open('../day_7.txt', 'r') as f:
-    step1 = (line.rstrip(".\n").split(" bags contain") for line in f)
-    step2 = ((t1, [x.split() for x in t2.split(",")]) for t1, t2 in step1)
-    rules = {t1: [(int(l[0]), " ".join(l[1:3])) for l in t2 if l[0].isdecimal()] for t1, t2 in step2}
+    rules = { s1: [(int(n), b) for n,b in re.findall("(\d+) ([a-z ]+) bags?", s2)] for s1, s2 in
+            (line.rstrip(".\n").split(" bags contain") for line in f) }
 
 def part1(bag):
     s = set() 
