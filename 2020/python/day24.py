706bfef235eaab956097a48568bbe070fc20c903
Luis Duarte <luis_duarte@outlook.com>
2021-01-06 19:34:24 +0000

    Improvements to days 17, 24 and 25 (python)

--- 2020/python/day24.py
+++ 2020/python/day24.py
@@ -1,47 +1,31 @@
-moves = {'e': (1,-1,0), 'w': (-1,1,0), 'ne': (1,0,-1), 'nw': (0,1,-1), 'se': (0,-1,1), 'sw': (-1,0,1) }
+from collections import Counter
 
 def translate(s):
-    x, y, z = 0, 0, 0
-    i = 0
+    i, p = 0, 0
     while i < len(s):
         if s[i] == 'n' or s[i] == 's':
-            mov = moves[s[i:i+2]]
+            p += directions[s[i:i+2]]
             i += 2
         else:
-            mov = moves[s[i]]
+            p += directions[s[i]]
             i += 1
-        x += mov[0]
-        y += mov[1]
-        z += mov[2]
-    return (x, y, z)
+    return p
 
 def get_neighbors(tile):
-    x, y, z = tile
-    return {(x+i, y+j, z+k) for i,j,k in moves.values()}
+    return (tile + d for d in directions.values())
 
-black_tiles = set()
+# https://www.redblobgames.com/grids/hexagons/
+directions = {'e': 2, 'w': -2, 'ne': 1+1j, 'nw': -1+1j, 'se': 1-1j, 'sw': -1-1j }
+
 with open('../day_24.txt') as f:
-    for line in f:
-        tile = translate(line.rstrip())
-        if tile in black_tiles:
-            black_tiles.remove(tile)
-        else:
-            black_tiles.add(tile)
+    tile_count = Counter(translate(line.rstrip()) for line in f)
+    black_tiles = set(t for t, c in tile_count.items() if c % 2 == 1)
 
 print('part 1:', len(black_tiles))
 
-
 for _ in range(100):
-    next_black_tiles = set()
-    white_tiles_to_flip = set()
-    for tile in black_tiles:
-        neighbors = get_neighbors(tile)
-        if 0 < len(black_tiles & neighbors) < 3:
-            next_black_tiles.add(tile)
-        for white_tile in neighbors - black_tiles:
-            if 2 == len(black_tiles & get_neighbors(white_tile)):
-                white_tiles_to_flip.add(white_tile)
-    black_tiles = next_black_tiles | white_tiles_to_flip
+    neighbors = Counter(n_tile for tile in black_tiles for n_tile in get_neighbors(tile))
+    black_tiles = {t for t, c in neighbors.items() if c == 2 or (t in black_tiles and c == 1)}
 
 print('part 2:', len(black_tiles))
 
--- 2020/python/day25.py
+++ 2020/python/day25.py
@@ -1,12 +1,10 @@
 door_pub_key = 14012298
 card_pub_key = 74241
 
-def find_loop_value(n):
-    i = 1
-    while pow(7, i, 20201227) != n:
-        i += 1
-    return i
+loop, n = 0, 1
+while n != door_pub_key:
+    n = n*7 % 20201227
+    loop += 1
 
-encryption_key = pow(card_pub_key, find_loop_value(door_pub_key), 20201227)
-print('part 1:', encryption_key)
+print('part 1:', pow(card_pub_key, loop, 20201227))
 
